9e9061a9de3d002fd92aeb01db256d62a37b785e
Greg <gbh346@gmail.com>
2018-02-09 05:06:33 -0500

    better names / sprite is only allocated if an image can be attacched

--- sprite.c
+++ sprite.c
@@ -2,8 +2,20 @@
 
 const char* _placeHolder = "res/placeholder_sprite.png";
 
-struct Sprite* CreateSprite(SDL_Renderer* renderer, int x, int y, int w, int h, const char* filename)
+struct Sprite* Sprite_Create(SDL_Renderer* renderer, int x, int y, int w, int h, const char* filename)
 {
+
+    SDL_Surface* surface = IMG_Load(filename);
+    
+    if(surface == NULL){
+        fprintf(stderr, "IMG_LOAD returned NULL, IMG_Error: %s\n", IMG_GetError());
+        surface = IMG_Load(_placeHolder);
+        if(surface == NULL){
+            fprintf(stderr,"placeholder_sprite also failed to load, IMG_Error: %s\n", IMG_GetError());
+            return NULL;
+        }
+    }
+
     struct Sprite* tmpSprite = (struct Sprite*)malloc(sizeof(struct Sprite));
     tmpSprite->x = x;
 	tmpSprite->y = y;
@@ -11,20 +23,13 @@ struct Sprite* CreateSprite(SDL_Renderer* renderer, int x, int y, int w, int h, 
 	tmpSprite->h = h;
     tmpSprite->filename = filename;
 
-    SDL_Surface* surface = IMG_Load(tmpSprite->filename);
-
-    if(surface == NULL){
-        fprintf(stderr, "IMG_LOAD returned NULL, IMG_Error: %s\n", IMG_GetError());
-        surface = IMG_Load(_placeHolder);
-        if(surface == NULL){
-            fprintf(stderr,"placeholder_sprite also failed to load, IMG_Error: %s\n", IMG_GetError());
-        }
-    }
-
     tmpSprite->texture = SDL_CreateTextureFromSurface(renderer, surface);
 
     if(tmpSprite->texture == NULL){
         fprintf(stderr, "Failed to create texture from surface! SDL_Error: %s\n", SDL_GetError());
+        SDL_FreeSurface(surface);
+        free(tmpSprite);
+        return NULL;
     }
 
     SDL_FreeSurface(surface);
@@ -32,14 +37,14 @@ struct Sprite* CreateSprite(SDL_Renderer* renderer, int x, int y, int w, int h, 
     return tmpSprite;
 }
 
-void DrawSprite(struct Sprite* sprite, SDL_Renderer* renderer)
+void Sprite_Draw(struct Sprite* sprite, SDL_Renderer* renderer)
 {
 	SDL_Rect tmpRect = { sprite->x, sprite->y, sprite->w, sprite->h };
 	SDL_RenderCopy(renderer, sprite->texture, NULL, &tmpRect);
 	return;
 }
 
-void FreeSprite(struct Sprite* sprite)
+void Sprite_Free(struct Sprite* sprite)
 {
     SDL_DestroyTexture(sprite->texture);
     free(sprite);
